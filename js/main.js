c106b731e27e83487dc5db4d148581b2b59e5aac
test <kimhjona@gmail.com>
2018-04-09 15:54:16 -0700

    pushing to gh-pages

--- js/main.js
+++ js/main.js
@@ -43,10 +43,10 @@ const toEmailPage = () => {
   const capturedEmail = $("#startPageEmailInput input").val();
   const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
-  // if (!regex.test(capturedEmail)) {
-  //   alert("Please enter a valid email address");
-  //   return;
-  // }
+  if (!regex.test(capturedEmail)) {
+    alert("Please enter a valid email address");
+    return;
+  }
 
   $("#linkToShare").addClass("smaller-margin");
   $("#container-largest-text").addClass("mt-5");
